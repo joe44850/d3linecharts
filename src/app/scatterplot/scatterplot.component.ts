08a264b7deaa015ef1eaeb7daf857dc177013611
Joe Linstrum <jlinstrum@NEW-INNOV.COM>
2017-03-17 21:23:23 +0100

    Added scatter plots

--- src/app/scatterplot/scatterplot.component.ts
+++ src/app/scatterplot/scatterplot.component.ts
@@ -30,7 +30,7 @@ export class ScatterplotComponent implements OnInit, OnChanges {
   public studentNames: any;
   private selectedStudents: any;
   private selectedStudentsData: any;
-  private lines: any;  
+  private lines: any = [];  
 
   public gridVisible: boolean;
   public selectedStudentIds: any = ["Average"];
@@ -38,7 +38,7 @@ export class ScatterplotComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit() {    
-    /* change this to not reference example data */
+    /* change this to not reference example data */    
     this.data = exampleData;
     this.height = 500;
     this.width = 1000;
@@ -68,6 +68,8 @@ export class ScatterplotComponent implements OnInit, OnChanges {
     this.yScale = d3.scaleLinear().range([height - margin.top, margin.bottom]).domain([0,5]);
     let xAxis = d3.axisBottom(this.xScale);
     let yAxis = d3.axisLeft(this.yScale);
+    this.xAxis = xAxis;
+    this.yAxis = yAxis;
     this.x = d3.scaleTime().range([0, width]);
     this.y = d3.scaleLinear().range([height, 0]);
 
@@ -80,35 +82,38 @@ export class ScatterplotComponent implements OnInit, OnChanges {
       .attr("transform", "translate(" + (margin.left) + ",0)")
       .call(yAxis);   
                
-  }  
+  } 
+
+  removeLines(){
+    if(!this.lines){return;} 
+    let i = 0;       
+    for(var studentId in this.lines){
+      var inArray = this.selectedStudentIds.indexOf(studentId);
+      if(inArray == -1){        
+        this.lines[studentId].remove();        
+        delete this.lines[studentId];
+      }
+      else{
+        console.log(`Index of ${studentId}: `+this.lines.indexOf(studentId));
+      }
+      i++;
+    }
+  } 
 
   updateChart(){
-    if(this.lines){
-      this.lines.forEach((d,i)=>{
-        var inArray = false;
-        for(var key in this.selectedStudentIds){
-          var student = this.selectedStudents[key];
-          console.log(`${key} == ${d.id} ? `);
-          if(student == d.id){
-            inArray = true;
-            break;
-          }
-        }
-        if(!inArray){ 
-          try{
-            d.remove();
-          }
-          catch(e){}
-        };
-      });
-    }
-    
+    this.removeLines();     
     if(!this.selectedStudentsData){ return; }    
-    this.dataGroup = d3.nest()
+    this.updateLineChart();               
+  }
+
+  updateLineChart(){
+    return new Promise((resolve)=>{
+      this.dataGroup = d3.nest()
         .key(function(d){
           return d["student"];
         })
     .entries(this.selectedStudentsData);
+    
     let x = this.x;
     let y = this.y;
     let xScale = this.xScale;
@@ -122,41 +127,84 @@ export class ScatterplotComponent implements OnInit, OnChanges {
         return yScale(d["avgScore"]);
       });
     let svg = this.svg;
-    let lines = [];
-    let currentLines = this.lines;
+    var _lines = [];
+    let current_lines = this.lines;  
+    
     var t = svg.transition()
             .duration(500)
-            .ease(d3.easeLinear)            
+            .ease(d3.easeLinear);           
 
-    this.dataGroup.forEach(function(d, i) { 
-        let inArray = false;
-        if(currentLines){
-          for(var e of lines){            
-            if(e.id == d.key){
-              lines.push(e); 
-              inArray = true;
-              break;
-            }
-          };
-        }
-        if(!inArray){
-          var line = svg.append('svg:path')
-          .attr('d', lineGen(d.values))
-          .attr('stroke', colors[i])
-          .attr('stroke-width', 2)
-          .attr('fill', 'none')
-          .attr('id', d.key)
-          .attr("stroke-dasharray", function(d){ return this.getTotalLength() })
-          .attr("stroke-dashoffset", function(d){ return this.getTotalLength() }); 
-          line.transition(t)
-            .attr("stroke-dashoffset", 0);
-          line.id = d.key;
-          lines.push(line);          
-        }
-                
-       });
-      this.lines = lines;
-  }  
+    this.dataGroup.forEach(function(d, i) {                 
+      let inArray = (d.key in this.lines);           
+      if(!inArray){          
+        var line = svg.append('svg:path')
+        .attr('d', lineGen(d.values))
+        .attr('stroke', colors[i])
+        .attr('stroke-width', 2)
+        .attr('fill', 'none')
+        .attr('id', d.key)
+        .attr("stroke-dasharray", function(d){ return this.getTotalLength() })
+        .attr("stroke-dashoffset", function(d){ return this.getTotalLength() }); 
+        line.transition(t)
+          .attr("stroke-dashoffset", 0);
+        line.id = d.key;
+        _lines[line.id] = line; 
+        this.lines[line.id] = line;  
+        this.updateScatterPlots(d);          
+      } 
+      else{
+        
+      }               
+    },this);
+      
+      return resolve();
+    });
+  } 
+
+  updateScatterPlots(d:any){
+    return new Promise((resolve)=>{
+      let svg = this.svg;      
+      let width = this.width;
+      let height = this.height;      
+      let xAxis = this.xAxis;
+      let yAxis = this.yAxis;       
+      var color = d3.scaleOrdinal(d3.schemeCategory10);
+      let margin = this.margin;      
+      let xScale = d3.scaleLinear().range([margin.left, width - margin.right]).domain([1.1,4.3]);
+      let yScale = d3.scaleLinear().range([height - margin.top, margin.bottom]).domain([0,5]);      
+      
+      let dataScatter = this.setDataScatter(d);        
+      svg.selectAll("circle")
+        .data(dataScatter)
+        .enter().append("circle")
+        .attr("class", "dot")        
+        .attr("r", 3.5)              
+        .attr("cx", function (d,i) { return xScale(d[0]); } )
+        .attr("cy", function (d) { return yScale(d[1]); } )
+        .style("fill", function(d) { return color("fee"); });      
+
+      return resolve(true);     
+      });    
+    
+  }
+
+  setDataScatter(studentData){
+    console.log(studentData);   
+    let retVal = [];
+    for(let key in studentData.values){
+      let item =  (studentData.values[key]);
+      let scores = item.scores;
+      let year = item.year;
+      let scoreArray = [];
+      if(!scores){ continue;}
+      for(let i=0; i<scores.length; i++){
+        scoreArray[0] = year;
+        scoreArray[1] = scores[i];
+        retVal.push(scoreArray);
+      }
+    }
+    return retVal;
+  }
 
   updateSelectedStudents(studentIds){
     this.selectedStudentIds = studentIds;    
@@ -168,9 +216,9 @@ export class ScatterplotComponent implements OnInit, OnChanges {
           studentData.push(item);
         }
       });      
-      this.selectedStudentsData = studentData;
+      this.selectedStudentsData = studentData;      
       return resolve();
-    });
+    });   
   }
 
   updateGrid(){  
@@ -207,8 +255,7 @@ export class ScatterplotComponent implements OnInit, OnChanges {
         studentNames.push(item.student);
       }
     });
-    this.studentNames = studentNames.sort();
-    console.log(this.studentNames);
+    this.studentNames = studentNames.sort();    
   }
 }
 
